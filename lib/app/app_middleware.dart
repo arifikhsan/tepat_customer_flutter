362a1f5e7fee48a775df8fb31014984c216a0ae4
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-07-31 11:30:43 +0700

    impl

--- lib/app/app_middleware.dart
+++ lib/app/app_middleware.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tepat_customer_flutter/config/injection/injection.dart';
-import 'package:tepat_customer_flutter/features/dashboard/data/repositories/dashboard_repository.dart';
+import 'package:tepat_customer_flutter/features/dashboard/data/repositories/dashboard_repository_impl.dart';
 import 'package:tepat_customer_flutter/features/dashboard/presentation/bloc/best_engineer_bloc.dart';
 
 class AppMiddleware extends StatelessWidget {
@@ -15,7 +15,7 @@ class AppMiddleware extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) => BestEngineerBloc(
-            repository: getIt<DashboardRepository>(),
+            repository: getIt<DashboardRepositoryImpl>(),
           ),
         ),
       ],
